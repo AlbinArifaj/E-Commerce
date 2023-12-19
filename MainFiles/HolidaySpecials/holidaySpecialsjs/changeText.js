c8117942ee2ca341fc006e884eccb5a26033efd9
adea-gerguri <adea.gerguri@student.uni-pr.edu>
2023-12-19 21:19:56 +0100

    IMG folder

---
+++ MainFiles/HolidaySpecials/holidaySpecialsjs/changeText.js
@@ -0,0 +1,6 @@
+function changeText() {
+    document.getElementById('text-to-change').innerHTML = 'Get our Christmas discount!';
+    }
+    function resetText(element) { 
+    element.innerHTML = "Order Dior's Advent Calendar";
+    }
