8be14235041b89df391f152e3bf9a78bbd93f0d1
adrianmehaj <adrianmehaj123@gmail.com>
2023-12-24 04:13:20 +0100

    ;;

--- MainFiles/KidsPage/script.js
+++ MainFiles/KidsPage/script.js
@@ -1,11 +1,11 @@
 
-let popup = document.getElementById('popup')
+let popup = document.getElementById('popup');
 
 function openPopup(){
-  $('html').css('overflow-y','hidden')
+  $('html').css('overflow-y','hidden');
 
-  popup.classList.add('open-popup')
-  let firstParagraph1 =  document.querySelectorAll(".linkToBuy")
+  popup.classList.add('open-popup');
+  let firstParagraph1 =  document.querySelectorAll(".linkToBuy");
   firstParagraph1.forEach(element=>{
       element.addEventListener("click",(event)=>{
         event.preventDefault();
@@ -14,7 +14,7 @@ function openPopup(){
         var elmentName = clickedElement.querySelector('.firstParagraph').innerHTML;
         var elementPrice = clickedElement.querySelector('.qmimi').innerHTML;
 
-        document.querySelector(".productImage").src=selectImage
+        document.querySelector(".productImage").src=selectImage;
         document.querySelector(".productName").innerHTML = elmentName;
         document.querySelector(".prductPrice").innerHTML = elementPrice;
 
@@ -24,6 +24,6 @@ function openPopup(){
 
  
 function closePopup(){
-  popup.classList.remove('open-popup')
-  $('html').css('overflow-y','scroll')
+  popup.classList.remove('open-popup');
+  $('html').css('overflow-y','scroll');
 }
