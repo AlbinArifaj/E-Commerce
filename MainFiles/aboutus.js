6638080422d3e2a483e720a249b30e7c5a1f7bb1
Besarta Mustafa <besartamustafa49@gmail.com>
2023-12-28 23:09:20 +0100

    modified:   MainFiles/Aboutus.css 	modified:   MainFiles/Aboutus.html 	modified:   MainFiles/aboutus.js 	new file:   MainFiles/canvas.css

--- MainFiles/aboutus.js
+++ MainFiles/aboutus.js
@@ -27,22 +27,43 @@
 //     };
 // });
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Get the canvas element
-    var canvas = document.getElementById('myCanvas');
-    var ctx = canvas.getContext('2d');
-
-    // Create an image element
-    var img = new Image();
-
-    // Replace 'path/to/your/image.jpg' with the actual path to your image file
-    img.src = 'path/to/your/image.jpg';
-
-    // Wait for the image to load
-    img.onload = function () {
-        // Draw the image on the canvas
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    };
-});
-
-
+(function(){
+    c1 = document.getElementById("slide1");
+    c2 = document.getElementById("slide2");
+    $ = c1.getContext("2d");
+    $$ = c2.getContext("2d");
+    
+    var i = 1; 
+    var r = 0; 
+    
+  img = new Image();
+    
+  img.src =  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/131045/slideshow.jpg?" + new Date().getTime();
+  
+  img.onload = function() {
+      $.drawImage(img,0,0,320,240,0,0,320,240);
+      $$.drawImage(img,0,240,320,240,9999,0,320,240);
+  };
+    
+  var t = setInterval(function() {
+      var one = (i - 1) * 240;
+      if (i == 1 || i == 2)
+        var two = i * 240;
+        else
+        two = 0;
+    
+      $.drawImage(img,0,one,320,240,0,0,320,240);
+      c2.width = c2.width;
+      $$.beginPath();
+      $$.arc(160, 120, r, 0, Math.PI * 2, false);
+      $$.clip();
+      $$.drawImage(img,0,two,320,240,0,0,320,240);
+    
+      r = r + 2;
+      if (r > 200) {
+        r = 0;
+        i++;
+      }
+      if (i > 3) i = 1;
+    }, 40);
+  })();
